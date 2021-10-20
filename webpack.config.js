80ec7c078d273f0437eb64fb60233b65464bd182
alexdor <alexandros@dorodouli.com>
2021-10-20 15:38:56 +0000

    Deploying to gh-pages from @ donkeyclip/motorcortex-code-typing@4b119e065cd547f6359d2df303aa6292d8f81a83 🚀

--- webpack.config.js
+++ webpack.config.js
@@ -1,76 +1,74 @@
-const path = require( 'path' );
-const webpack = require( 'webpack' );
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
+  context: path.resolve(__dirname),
 
-	context: path.resolve( __dirname ),
+  entry: "./demo/script.js",
 
-	entry: './demo/script.js',
+  resolve: {
+    extensions: [".js"],
+    modules: [path.resolve("./"), "node_modules"],
+  },
+  output: {
+    filename: "bundle.js",
+    // the output bundle
 
-	resolve: {
-		extensions: [
-			'.js'
-		],
-		modules: [
-			path.resolve( './' ),
-			'node_modules'
-		]
-	},
-	output: {
-		filename: 'bundle.js',
-		// the output bundle
+    path: path.resolve("./demo/"),
+  },
 
-		path: path.resolve('./demo/')
+  devtool: "cheap-module-eval-source-map",
+  mode: "development",
 
-	},
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        use: ["babel-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader",
+            options: { sourceMap: true }, // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader",
+            options: { sourceMap: true }, // translates CSS into CommonJS
+          },
+        ],
+      },
+    ],
+  },
 
-	devtool: 'cheap-module-eval-source-map',
-	mode: 'development',
+  plugins: [
+    new webpack.ProvidePlugin({
+      Promise: "es6-promise",
+      fetch:
+        "imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch",
+    }),
 
-	module: {
-		rules: [
-			{
-				test: /\.js?$/,
-				use: [ 'babel-loader' ],
-				exclude: /node_modules/
-			},
-			{
-				test: /\.css$/,
-				use: [ 'style-loader', 'css-loader' ]
-			},
-			{
-				test: /\.scss$/,
-				use: [ {
-					loader: "style-loader", options: { sourceMap: true } // creates style nodes from JS strings
-				}, {
-					loader: "css-loader", options: { sourceMap: true } // translates CSS into CommonJS
-				}, {
-					loader: "sass-loader", options: { sourceMap: true } // compiles Sass to CSS
-				} ]
-			}
-		]
-	},
+    new webpack.HotModuleReplacementPlugin(),
+    // enable HMR globally
 
-	plugins: [
+    new webpack.NamedModulesPlugin(),
+    // prints more readable module names in the browser console on HMR updates
 
-		new webpack.ProvidePlugin( { 'Promise': 'es6-promise', 'fetch': 'imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch' } ),
+    new webpack.NoEmitOnErrorsPlugin(),
+    // do not emit compiled assets that include errors
+  ],
 
-		new webpack.HotModuleReplacementPlugin( ),
-		// enable HMR globally
-
-		new webpack.NamedModulesPlugin( ),
-		// prints more readable module names in the browser console on HMR updates
-
-		new webpack.NoEmitOnErrorsPlugin( ),
-		// do not emit compiled assets that include errors
-
-	],
-
-	devServer: {
-		watchContentBase: false, // initiate a page refresh if static content changes
-		host: '0.0.0.0',
-		port: 8080,
-		historyApiFallback: false,
-		hot: false
-	}
+  devServer: {
+    watchContentBase: false, // initiate a page refresh if static content changes
+    host: "0.0.0.0",
+    port: 8080,
+    historyApiFallback: false,
+    hot: false,
+  },
 };
